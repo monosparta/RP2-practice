a70e701268daead71bb46818f7ab5bc264ce8736
bohan1109 <4A790162@stust.edu.tw>
2022-03-17 18:24:00 +0800

    fix: :bug: fix multiple bug
    change thread to process level, new mysql insert action, get 10 records

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from time import sleep
 from urllib import response
 from flask import Flask, render_template, request, jsonify,json,url_for
 from flask_sqlalchemy import SQLAlchemy
@@ -7,6 +8,7 @@ from sqlalchemy import false
 
 import paho.mqtt.client as mqtt
 import threading
+from multiprocessing import Process
 import mysql.connector
 
 
@@ -14,13 +16,13 @@ app = Flask(__name__)
 
 # MySql datebase
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/test"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/flask"
 
 db = SQLAlchemy(app)
 
 # 模型( model )定義
-class Product4(db.Model):
-    __tablename__ = 'product4'
+class dh11(db.Model):
+    __tablename__ = 'dh11'
     id = db.Column(db.Integer, primary_key=True)
     temp = db.Column(
         db.VARCHAR(30),  nullable=False)
@@ -35,31 +37,13 @@ class Product4(db.Model):
         self.temp = temp
         self.humi = humi
         self.time = time
-
-
-
-
-# query = Product1.query.filter_by(id='1').first()
-# print(query.temperature)
-# print(query.humidity)
-
-@app.route('/add',methods=["POST","GET"])
-def add():
-    sql = """
-    INSERT INTO product4(
-    temp, humi,time)
-    VALUES ('%s', '%s','%s')
-"""
-
-    db.engine.execute(sql)
-    return "yes"
     
 @app.route('/index')
 def index1():
     sql = """
-    select * from product4
+    select * from dh11 
     """
-    print(db.engine.execute(sql).fetchall())
+    # print(db.engine.execute(sql).fetchall())
     a = db.engine.execute(sql).fetchall()
     b = json.dumps([dict(r) for r in a])
     return render_template('index.html',data = b)
@@ -67,9 +51,9 @@ def index1():
 @app.route('/list')
 def list():
     sql = """
-    select * from product4
+    select * from (select * from dh11 order by id desc limit 10) as preProcess order by id;
     """
-    print(db.engine.execute(sql).fetchall())
+    # print(db.engine.execute(sql).fetchall())
     a = db.engine.execute(sql).fetchall()
     return json.dumps([dict(r) for r in a])
 @app.route('/')
@@ -89,27 +73,32 @@ def on_connect(client, userdata, flags, rc):
 
 mysqlConnection = None    
 def on_message(client, userdata, msg):
+    print('trigger on message')
     decodedMessage = str(msg.payload.decode("utf-8"))
     content = json.loads(decodedMessage)
+    print(content['temperature'], content['humidity'], content['time'])
     global mysqlConnection
     cursor = mysqlConnection.cursor()
-    cursor.execute('insert into <table name> (<fields>)\
-        values (<values>)')
+    queryString = 'insert into `DH11` (`temp`,`humi`,`time`) \
+        values ("%s", "%s", "%s");'%(str(content['temperature']), str(content['humidity']), str(content['time']))
+    # print(queryString)
+    cursor.execute(queryString)
+    mysqlConnection.commit()
 
 
 def mqttHandlerAndMysql():
     global mysqlConnection
-    mysqlConnection = mysql.connector.connect(host='192.168.168.112',
-                                         database='<db>',
-                                         user='<user>',
-                                         password='<password>')
+    mysqlConnection = mysql.connector.connect(host='127.0.0.1',
+                                         database='flask',
+                                         user='root',
+                                         password='')
         
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect("192.168.168.112", 1883, 60)
+    client.connect("127.0.0.1", 1883, 60)
     client.loop_forever()
 
 if __name__ == "__main__":
-    threading.Thread(target=mqttHandlerAndMysql).start()
-    app.run(debug=True)
+    Process(target=mqttHandlerAndMysql).start()
+    app.run(host='0.0.0.0', debug=True, )
