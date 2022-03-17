25a85fba2895319ce19055a484793461dc47ccc7
Hong-Shao <joee0511@gmail.com>
2022-03-17 12:57:31 +0800

    fix print syntax error

--- src/IoT/rpi2.py
+++ src/IoT/rpi2.py
@@ -7,7 +7,7 @@ try:
     while True:
         h, t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, GPIO_PIN)
         if h is not None and t is not None:
-            print('temp=%.1f*C, humidity=%.1\%'%(t, h))
+            print('temp=%.1f*C, humidity=%.1f%%'%(t, h))
         else:
             print('read error')
         time.sleep(0.05)
