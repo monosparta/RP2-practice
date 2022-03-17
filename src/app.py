56bbd25affc8c7b6e1e9dc7d84d9f7af1b76c388
Hong-Shao <joee0511@gmail.com>
2022-03-17 15:32:05 +0800

    feat: :sparkles: add new thread to do mqtt_on_message_handler, and sql_handler_to_mysql

--- src/app.py
+++ src/app.py
@@ -1,27 +1,115 @@
+from urllib import response
+from flask import Flask, render_template, request, jsonify,json,url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+from datetime import datetime
 
-db = SQLAlchemy()
+from sqlalchemy import false
+
+import paho.mqtt.client as mqtt
+import threading
+import mysql.connector
+
 
 app = Flask(__name__)
 
+# MySql datebase
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/laravel"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/test"
 
-db.init_app(app)
+db = SQLAlchemy(app)
 
+# 模型( model )定義
+class Product4(db.Model):
+    __tablename__ = 'product4'
+    id = db.Column(db.Integer, primary_key=True)
+    temp = db.Column(
+        db.VARCHAR(30),  nullable=False)
+    humi = db.Column(
+        db.VARCHAR(30),  nullable=False)
+    time = db.Column(
+        db.VARCHAR(30), nullable=False)
+    
+
+
+    def __init__(self, temp, humi, time):
+        self.temp = temp
+        self.humi = humi
+        self.time = time
+
+
+
+
+# query = Product1.query.filter_by(id='1').first()
+# print(query.temperature)
+# print(query.humidity)
+
+@app.route('/add')
+def add():
+    sql = """
+    INSERT INTO product4(
+    temp, humi,time)
+    VALUES ('20', '90','03/17 14:39:24')
+"""
+
+    db.engine.execute(sql)
+    return "yes"
+    
+@app.route('/index')
+def index1():
+    sql = """
+    select * from product4
+    """
+    print(db.engine.execute(sql).fetchall())
+    a = db.engine.execute(sql).fetchall()
+    b = json.dumps([dict(r) for r in a])
+    return render_template('index.html',data = b)
+
+@app.route('/list')
+def list():
+    sql = """
+    select * from product4
+    """
+    print(db.engine.execute(sql).fetchall())
+    a = db.engine.execute(sql).fetchall()
+    return json.dumps([dict(r) for r in a])
 @app.route('/')
 def index():
+    # Create data
+    #a = Product1.query.all()
+    db.create_all()
+    #b = json.dumps([dict(r) for r in a])
+    
+#list(a.id())
+    return "ok"
 
-    sql_cmd = """
-        select *
-        from lockers
-        """
 
-    query_data = db.engine.execute(sql_cmd)
-    print(query_data)
-    return 'ok'
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("test")
 
+mysqlConnection = None    
+def on_message(client, userdata, msg):
+    decodedMessage = str(msg.payload.decode("utf-8"))
+    content = json.loads(decodedMessage)
+    global mysqlConnection
+    cursor = mysqlConnection.cursor()
+    cursor.execute('insert into <table name> (<fields>)\
+        values (<values>)')
+
+
+def mqttHandlerAndMysql():
+    global mysqlConnection
+    mysqlConnection = mysql.connector.connect(host='192.168.168.112',
+                                         database='<db>',
+                                         user='<user>',
+                                         password='<password>')
+        
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect("192.168.168.112", 1883, 60)
+    client.loop_forever()
 
 if __name__ == "__main__":
-    app.run()
+    threading.Thread(target=mqttHandlerAndMysql).start()
+    app.run(debug=True)
