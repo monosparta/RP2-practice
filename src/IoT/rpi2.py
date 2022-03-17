a0066b846fa087b900feb8b43aaaaea84b3d6d4b
Hong-Shao <joee0511@gmail.com>
2022-03-17 13:32:04 +0800

    check mqtt reference success

--- src/IoT/rpi2.py
+++ src/IoT/rpi2.py
@@ -1,13 +1,30 @@
+import paho.mqtt.client as mqtt
+import json
+import datetime
 import time
 import Adafruit_DHT
+
+ 
+ISOTIMEFORMAT = '%m/%d %H:%M:%S'
+client = mqtt.Client()
+client.username_pw_set("jeams03","0303zz")
+client.connect('192.168.168.112', 1883)
  
 GPIO_PIN = 4
  
 try:
     while True:
-        h, t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, GPIO_PIN)
-        if h is not None and t is not None:
-            print('temp=%.1f*C, humidity=%.1f%%'%(t, h))
+        humidity, temperature = \
+            Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, GPIO_PIN)
+        mqttPayload = {
+            'temperature': '%.1f'%temperature, 
+            'humidity': '%.1f'%humidity,
+            'time': datetime.datetime.now().strftime(ISOTIMEFORMAT),
+        }
+        if humidity is not None and temperature is not None:
+            # print('temp=%.1f*C, humidity=%.1f%%'%(temperature, humidity))
+            print(mqttPayload)
+            # client.publish("test", json.dumps(mqttPayload))
         else:
             print('read error')
         time.sleep(0.05)
