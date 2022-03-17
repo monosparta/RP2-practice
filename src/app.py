d877758ed9d3a470f45b9efbdb17302b4201f257
bohan1109 <4A790162@stust.edu.tw>
2022-03-17 14:57:56 +0800

    push new file

--- src/app.py
+++ src/app.py
@@ -1,27 +1,83 @@
+from urllib import response
+from flask import Flask, render_template, request, jsonify,json,url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+from datetime import datetime
 
-db = SQLAlchemy()
+from sqlalchemy import false
+
+
 
 app = Flask(__name__)
 
+# MySql datebase
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/laravel"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/test"
 
-db.init_app(app)
+db = SQLAlchemy(app)
 
+# 模型( model )定義
+class Product4(db.Model):
+    __tablename__ = 'product4'
+    id = db.Column(db.Integer, primary_key=True)
+    temp = db.Column(
+        db.VARCHAR(30),  nullable=False)
+    humi = db.Column(
+        db.VARCHAR(30),  nullable=False)
+    time = db.Column(
+        db.VARCHAR(30), nullable=False)
+    
+
+
+    def __init__(self, temp, humi, time):
+        self.temp = temp
+        self.humi = humi
+        self.time = time
+
+
+
+
+# query = Product1.query.filter_by(id='1').first()
+# print(query.temperature)
+# print(query.humidity)
+
+@app.route('/add')
+def add():
+    sql = """
+    INSERT INTO product4(
+    temp, humi,time)
+    VALUES ('20', '90','03/17 14:39:24')
+"""
+
+    db.engine.execute(sql)
+    return "yes"
+    
+@app.route('/index')
+def index1():
+    sql = """
+    select * from product4
+    """
+    print(db.engine.execute(sql).fetchall())
+    a = db.engine.execute(sql).fetchall()
+    b = json.dumps([dict(r) for r in a])
+    return render_template('index.html',data = b)
+
+@app.route('/list')
+def list():
+    sql = """
+    select * from product4
+    """
+    print(db.engine.execute(sql).fetchall())
+    a = db.engine.execute(sql).fetchall()
+    return json.dumps([dict(r) for r in a])
 @app.route('/')
 def index():
-
-    sql_cmd = """
-        select *
-        from lockers
-        """
-
-    query_data = db.engine.execute(sql_cmd)
-    print(query_data)
-    return 'ok'
-
+    # Create data
+    #a = Product1.query.all()
+    db.create_all()
+    #b = json.dumps([dict(r) for r in a])
+    
+#list(a.id())
+    return "ok"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
