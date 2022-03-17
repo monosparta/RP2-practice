58d7a882947c943991593b92c17b07e245bae980
Hong-Shao <joee0511@gmail.com>
2022-03-17 11:31:07 +0800

    try to get DHT11 data

---
+++ src/IoT/rpi2.py
@@ -0,0 +1,16 @@
+import time
+import Adafruit_DHT
+ 
+GPIO_PIN = 4
+ 
+try:
+    while True:
+        h, t = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, GPIO_PIN)
+        if h is not None and t is not None:
+            print('temp=%.1f*C, humidity=%.1\%'%(t, h))
+        else:
+            print('read error')
+        time.sleep(0.05)
+        
+except KeyboardInterrupt:
+    print('keyboard interrupt')
