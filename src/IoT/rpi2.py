bb80002d52b5fa443eb4f61bc1d79e26f3f95772
Hong-Shao <joee0511@gmail.com>
2022-03-21 10:17:33 +0800

    feat: :sparkles: try to write LCD light

--- src/IoT/rpi2.py
+++ src/IoT/rpi2.py
@@ -3,6 +3,13 @@ import json
 import datetime
 import time
 import Adafruit_DHT
+import LCD1602
+import time
+
+LCD1602.init(0x27, 1)   # init(slave address, background light)
+LCD1602.write(0, 0, 'Hi....')
+LCD1602.write(1, 1, 'Cherish the time')
+time.sleep(2)
 
  
 ISOTIMEFORMAT = '%m/%d %H:%M:%S'
@@ -13,6 +20,7 @@ GPIO_PIN = 4
  
 try:
     while True:
+        LCD1602.clear()
         humidity, temperature = \
             Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, GPIO_PIN)
         mqttPayload = {
@@ -20,13 +28,19 @@ try:
             'humidity': '%.1f'%humidity,
             'time': datetime.datetime.now().strftime(ISOTIMEFORMAT),
         }
+        
         if humidity is not None and temperature is not None:
             # print('temp=%.1f*C, humidity=%.1f%%'%(temperature, humidity))
             print(mqttPayload)
             client.publish("test", json.dumps(mqttPayload))
         else:
             print('read error')
-        time.sleep(0.05)
+            
+        LCD1602.write(0, 0,"Date: {}".format(time.strftime("%Y/%m/%d")))
+        LCD1602.write(0, 1,"Time: {}".format(time.strftime("%H:%M:%S")))
+        time.sleep(0.1)
         
 except KeyboardInterrupt:
     print('keyboard interrupt')
+finally:
+    LCD1602.clear()
