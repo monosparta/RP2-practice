9a5c82e7e9b94a88e6005e685386497f40975832
Hong-Shao <joee0511@gmail.com>
2022-03-22 14:35:17 +0800

    modify the input data to i2c

--- src/IoT/rpi2.py
+++ src/IoT/rpi2.py
@@ -15,16 +15,18 @@ def i2cHandler():
     def on_message(client, userdata, msg):
         decodedMessage = str(msg.payload.decode("utf-8"))
         content = json.loads(decodedMessage)
-        # print(content['temperature'], content['humidity'], content['time'])
-        LCD1602.write(0, 0,"<total 16>")
-        LCD1602.write(0, 1,"<en words>")
+        # print(content)
+        message = content['message']
+        # print(message)
+        LCD1602.write(0, 0, '%-16s'%(message[0:16]))
+        if len(message) > 32:
+            LCD1602.write(0, 1, '%-12s....'%(message[16:28]))
+        else:
+            LCD1602.write(0, 1, '%-16s'%(message[16:32]))
 
     try:
         # using "$ i2cdetect -y 1" to look for i2c-address
         LCD1602.init(0x3f, 1)   # init(slave address, background light)
-        LCD1602.write(0, 0, 'Hi....')
-        LCD1602.write(1, 1, 'Cherish the time')
-        time.sleep(10)
         client.subscribe('i2c')
         client.on_message = on_message
         client.loop_forever()
@@ -43,7 +45,7 @@ def dh11Detect():
             }
             
             if humidity is not None and temperature is not None:
-                print(mqttPayload)
+                # print(mqttPayload)
                 client.publish("dh11", json.dumps(mqttPayload))
             else:
                 print('read error')
